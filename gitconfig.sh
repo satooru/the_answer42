fea7b723ba5fd4e63b34df528adb18f11cc60ffc
satooru <16670493+satooru@users.noreply.github.com>
2021-05-28 23:55:13 +0000

    tracking gitconfig.sh

---
+++ gitconfig.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git config --global diff.tool vimdiff
+git config --global difftool.prompt false
+git config --global alias.d difftool
+git config --global alias.s status
+git config --global alias.l log
+echo 'git configurado'
